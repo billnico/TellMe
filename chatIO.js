ccf2350ddae700395f2969a617cdd5d8976f1342
nicolas ngwira <ngwiranicolas@gmail.com>
2023-08-11 00:11:02 +0200

    added on message events for sending messages

--- chatIO.js
+++ chatIO.js
@@ -5,9 +5,24 @@ const http=require("http");
 const server=http.createServer(app);
 const io=socketIO(server);
 
+const activeUsers=[];
+const pendingMessages=[];
 
 io.on("connection",(socket)=>{
       
+      //data must include reciever-username, message and sender-name
+      socket.on("message",(data)=>{
+          if(activeUsers.includes(data.receiver)){
+              const sender=activeUsers.find((user)=>user.id===socket.id);
+              const messageData={
+                 sender:sender,
+                 message:data.message
+              }
+              io.to(data.receiver.id).emit("message",messageData);
+          }else{
+            pendingMessages.push(data);
+          }
+      });
 });
 
 module.exports=io;
