810485488fcceb859b405e7df139dbc6c7dbbbf5
nicolas ngwira <ngwiranicolas@gmail.com>
2023-08-11 00:37:56 +0200

    added checking for messages and adding active users when connecting

--- chatIO.js
+++ chatIO.js
@@ -9,18 +9,35 @@ const activeUsers=[];
 const pendingMessages=[];
 
 io.on("connection",(socket)=>{
-      
+      //new active user register with their name
+      //active fires first before check from client
+      socket.on("active",(name)=>{
+          const user={
+            id:socket.id,
+            name:name
+          };
+          activeUsers.push(user);
+      });
+      //when client connects they check for new message
+      socket.on("check",(data)=>{
+         if(pendingMessages.includes(data.name)){
+            const self=activeUsers.find((user)=>{user.name===data.name});
+            io.to(self.id).emit("message",messageData);
+         }
+      });
       //data must include reciever-username, message and sender-name
       socket.on("message",(data)=>{
-          if(activeUsers.includes(data.receiver)){
               const sender=activeUsers.find((user)=>user.id===socket.id);
+              const owner=activeUsers.find((user)=>user.name===data.receiver);
               const messageData={
                  sender:sender,
-                 message:data.message
+                 message:data.message,
+                 receiver:owner
               }
+              if(activeUsers.includes(data.receiver)){
               io.to(data.receiver.id).emit("message",messageData);
           }else{
-            pendingMessages.push(data);
+              pendingMessages.push(messageData);
           }
       });
 });
